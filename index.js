54c5cc58a7a5b641d3854074aa91695cd7f9d337
William Gotterer <william.gotterer@gmail.com>
2021-10-01 09:51:08 -0400

    commit tree de3439e86eb0c9be4c4ffb35e25315436cb7e7eb

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Add your code here
+
+
+function submitData(userUsername, userEmail){
+   return fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: userUsername,
+        email: userEmail
+    }),
+})
+     .then(function (response){
+         return response.json();
+    })
+     .then(object => {
+        const para = document.createElement("p")
+        para.innerHTML = object.id
+        document.body.appendChild(para)
+        console.log(object)
+
+     })
+     .catch(function(error){
+         const para = document.createElement("p")
+         
+         console.log(error)
+        para.innerHTML = error.message
+        document.body.appendChild(para)
+
+         
+     });
+
+
+      
+}
+
+
+
+
+// Headers for 'Content-Type' and 'Accept', both set to 'application/json'
+// A body with the name and email passed in as arguments to submitData. 
+// These should be assigned to name and email keys within an object. 
+// This object should then be stringified.
